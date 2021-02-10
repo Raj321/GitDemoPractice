bd430271b64a6a4ff0c861aaf01e333a5062d927
Rajesh <Raj0up@gmail.com>
2021-02-09 19:47:20 -0500

    added all original files

---
+++ src/main/java/com/gitdemo/gitpractice/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.gitdemo.gitpractice.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class MainController {
+
+    @GetMapping("/hello")
+    public  String HelloWorld(){
+
+        return "hello";
+    }
+}
